7c4aaca9e33fd9a584d7162037fd50194eef7a42
yuka1369 <okuda.yuka.ou0@is.naist.jp>
2022-01-03 21:53:56 +0900

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,6 +4,7 @@ function App() {
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
+                    <h1>ToDo App by okuda yuka</h1>
                     <TodoListCard />
                 </Col>
             </Row>
